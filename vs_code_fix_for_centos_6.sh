14dc3daefc1eafe07b2fbee18154769e482f7f83
mamiklos <marcel.miklosik@nokia.com>
2019-10-31 20:47:10 +0100

    Adding script/workaroud for running VS Code on CentOS 6.0+

---
+++ vs_code_fix_for_centos_6.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+#Note: Workaroud for running VS Code on CentOS 6.0+
+#      related to VS Code Remote Explorer -- as they need newer libraries   
+
+# get libc and static libs
+wget http://copr-be.cloud.fedoraproject.org/results/mosquito/myrepo-el6/epel-6-x86_64/glibc-2.17-55.fc20/glibc-2.17-55.el6.x86_64.rpm
+wget http://copr-be.cloud.fedoraproject.org/results/mosquito/myrepo-el6/epel-6-x86_64/glibc-2.17-55.fc20/glibc-common-2.17-55.el6.x86_64.rpm
+wget http://copr-be.cloud.fedoraproject.org/results/mosquito/myrepo-el6/epel-6-x86_64/glibc-2.17-55.fc20/glibc-devel-2.17-55.el6.x86_64.rpm
+wget http://copr-be.cloud.fedoraproject.org/results/mosquito/myrepo-el6/epel-6-x86_64/glibc-2.17-55.fc20/glibc-headers-2.17-55.el6.x86_64.rpm
+wget https://copr-be.cloud.fedoraproject.org/results/mosquito/myrepo-el6/epel-6-x86_64/glibc-2.17-55.fc20/glibc-utils-2.17-55.el6.x86_64.rpm
+wget https://copr-be.cloud.fedoraproject.org/results/mosquito/myrepo-el6/epel-6-x86_64/glibc-2.17-55.fc20/glibc-static-2.17-55.el6.x86_64.rpm
+
+# Install them with nodeps
+rpm -Uh --force --nodeps \
+glibc-2.17-55.el6.x86_64.rpm \
+glibc-common-2.17-55.el6.x86_64.rpm \
+glibc-devel-2.17-55.el6.x86_64.rpm \
+glibc-headers-2.17-55.el6.x86_64.rpm \
+glibc-static-2.17-55.el6.x86_64.rpm \
+glibc-utils-2.17-55.el6.x86_64.rpm
+
+# get  libstdc++
+wget https://copr-be.cloud.fedoraproject.org/results/mosquito/myrepo-el6/epel-6-x86_64/gcc-4.8.2-16.3.fc20/libstdc++-4.8.2-16.3.el6.x86_64.rpm
+wget https://copr-be.cloud.fedoraproject.org/results/mosquito/myrepo-el6/epel-6-x86_64/gcc-4.8.2-16.3.fc20/libstdc++-devel-4.8.2-16.3.el6.x86_64.rpm
+wget https://copr-be.cloud.fedoraproject.org/results/mosquito/myrepo-el6/epel-6-x86_64/gcc-4.8.2-16.3.fc20/libstdc++-static-4.8.2-16.3.el6.x86_64.rpm
+
+#Install them with nodeps
+rpm -Uh --force --nodeps\
+libstdc++-4.8.2-16.3.el6.x86_64.rpm \
+libstdc++-devel-4.8.2-16.3.el6.x86_64.rpm \
+libstdc++-static-4.8.2-16.3.el6.x86_64.rpm
